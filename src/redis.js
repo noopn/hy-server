e732cb0b7cdc5b4a185777ac56f1dbb75cdcefec
sunzhiqi <sunzhiqi@live.com>
2021-07-06 20:50:44 +0800

    hy-server

---
+++ src/redis.js
@@ -0,0 +1,39 @@
+const redis = require('redis');
+const client = redis.createClient(6379, 'localhost');
+
+client.on("ready", function (err) {
+  if (err) {
+    console.log("Error " + error);
+  } else {
+    console.log("redis ready");
+  }
+})
+
+client.on("connect", function (err) {
+  if (err) {
+    console.log("Error " + error);
+  } else {
+    console.log("redis connect");
+  }
+})
+
+const get = (key) => new Promise((resolve, reject) => {
+  client.get(key, (err, v) => {
+    if (err) reject('error');
+    resolve(v);
+  })
+})
+
+const set = (k, v) => client.set(k, v);
+
+// init 
+
+get('mode')
+  .then(v => {
+    if (v == null) set('mode', '1.1');
+  });
+
+module.exports = {
+  get,
+  set
+}
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const Koa = require('koa');
+const app = new Koa();
+const {
+  get,
+  set
+} = require('./redis');
+const server = require('http').createServer(app.callback());
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('用户连接上服务器')
+  // 向新链接的页面通知当前模式
+  get('mode').then(mode => {
+    socket.emit('mode', {
+      mode
+    });
+  })
+
+  socket.on("mode", ({
+    mode
+  }) => {
+    console.log(mode, '服务器接收到模式')
+    set('mode', mode)
+    get('mode')
+      .then(m => {
+        console.log('redis取到mode为', m)
+        io.sockets.emit('mode', {
+          mode: m
+        });
+      }).catch(err => {
+        io.sockets.emit('mode', {
+          mode: err
+        });
+      })
+  })
+  socket.on('disconnect', () => {
+    console.log('用户断开连接')
+  })
+});
+
+server.listen(3001, () => {
+  console.log('socket server in 3001 port')
+});
